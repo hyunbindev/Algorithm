5d03cd5e9d5fce7fcef3ed7bd4721e879e578c0b
김현빈 <132333588+hyunbindev@users.noreply.github.com>
2025-08-12 11:25:20 +0900

    Create 완주하지못한 선수.java

---
+++ programmers/lv1/완주하지못한 선수.java
@@ -0,0 +1,23 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42576
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String,Integer> map = new HashMap<>();
+        for(String c : completion){
+            Integer value = map.get(c);
+            if(value == null){
+                map.put(c,1);
+            }else{
+                map.put(c,value+1);
+            }
+        }
+        
+        for(String p : participant){
+            Integer value = map.get(p);
+            if(value == null || value == 0) return p;
+            
+            map.put(p,value-1);
+        }
+        return "";
+    }
+}
