763557cbbfd1649f5ff3b69306571eb8d7276b85
김현빈 <132333588+hyunbindev@users.noreply.github.com>
2025-10-14 15:09:02 +0900

    Create 스티커_모으기.java

---
+++ programmers/lv3/스티커_모으기.java
@@ -0,0 +1,26 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12971
+import java.util.*;
+class Solution {
+    public int solution(int sticker[]) {
+        if(sticker.length==1) return sticker[0];
+        if(sticker.length==2) return Math.max(sticker[0],sticker[1]);
+        
+        int[]dp1 = new int[sticker.length];
+        int[]dp2 = new int[sticker.length];
+        int answer = 0;
+        dp1[0]=sticker[0];
+        dp1[1] = Math.max(sticker[0], sticker[1]);
+        
+        dp2[0]=0;
+        dp2[1]=sticker[1];
+        for(int i=2; i<sticker.length-1; i++){
+            dp1[i]=Math.max(sticker[i]+dp1[i-2],dp1[i-1]);
+            answer=Math.max(answer,dp1[i]);
+        }
+        for(int i=2; i<sticker.length; i++){
+            dp2[i]=Math.max(sticker[i]+dp2[i-2],dp2[i-1]);
+            answer=Math.max(answer,dp2[i]);
+        }
+        return answer;
+    }
+}
